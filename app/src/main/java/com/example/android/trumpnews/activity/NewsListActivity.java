93c7917fe0e8c3a77c4c0ab2e5004118b955033a
Ammar SaadDine <ammar.as.89@gmail.com>
2018-04-03 23:31:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/trumpnews/activity/NewsListActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.trumpnews.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.trumpnews.R;
+
+public class NewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
---
+++ app/src/main/java/com/example/android/trumpnews/entity/NewsEntry.java
@@ -0,0 +1,8 @@
+package com.example.android.trumpnews.entity;
+
+/**
+ * Created by ammar_saaddine on 03.04.18.
+ */
+
+public class NewsEntry {
+}
